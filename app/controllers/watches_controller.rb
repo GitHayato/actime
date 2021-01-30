b770609e79e979e24eec9e995bc745a2bdd349e5
GitHayato <gabayt0_0@icloud.com>
2021-01-30 12:27:52 +0900

    feat: ラップ機能バックエンド

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -3,5 +3,11 @@ class WatchesController < ApplicationController
   end
 
   def new
+    @watches = Watch.all
+  end
+
+  def create
+    post = Post.create
+    render json:{ post: post }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  resources :watches, only: [:index, :new]
+  resources :watches, only: [:index, :new, :create]
 end
