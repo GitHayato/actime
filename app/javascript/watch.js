fe0bcac2556647c3199bd8cc674c6d6d88a8cd2e
GitHayato <gabayt0_0@icloud.com>
2021-02-03 09:20:56 +0900

    feat: js作動しないので一旦放置

--- app/javascript/watch.js
+++ app/javascript/watch.js
@@ -19,32 +19,57 @@ function stopWatch() {
 
     timerId = setTimeout(nowTime, 10);
   }
-
+  
   // 即時関数を定義して初期状態で呼び出し
   function stopWatchInitial() {
     // id="rap"ボタンののHTMLからinactiveというclassを除く
     rap.classList.add("inactive");
   };
-
+  
   function stopWatchRun() {
     // id="rap"のHTMLにinactiveというclassをつける
     rap.classList.remove("inactive");
   };
-
+  
   // この時点で呼び出すことで初期状態で呼び出されている状態になる
   stopWatchInitial();
 
+  function addRap() {
+    const XHR = new XMLHttpRequest();
+    const time = document.getElementById("time").innerHTML;
+    XHR.open("POST", "/watches", true);
+    XHR.responseType = "json"
+    XHR.onload = () => {
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      }
+      const content = XHR.response.watch;
+      console.log(content)
+      // const table = document.getElementById("time-table");
+      // const HTML = `
+      //     <tr>
+      //       <div class="data" data-id=${content.id}>
+      //         <td class="date">${content.id}</td>
+      //         <td class="time">${content.watch}</td>
+      //       </div>
+      //     </tr>`;
+      // table.insertAdjacentHTML("afterend", HTML);
+    };
+    XHR.send(time);
+  }
+  
   // id="start"ボタンがクリックされたときの挙動
-  start.addEventListener('click', function() {
+  start.addEventListener('click', (e) => {
     // inputタグのvalue属性が"START"のときの挙動
     if (start.value == 'START') {
       // ボタンの表示変更
       start.value = 'STOP';
       rap.value = 'RAP';
-      stopWatchRun();
-      // 
+      stopWatchRun(); 
       startTime = new Date();
-      nowTime()
+      nowTime();
+      e.preventDefault();
 
     // inputタグのvalue属性が"STOP"のときの挙動
     } else if (start.value == 'STOP') {
@@ -52,26 +77,28 @@ function stopWatch() {
       rap.value = 'RESET';
       clearTimeout(timerId)
       stoppingTime += Date.now() - startTime;
+      e.preventDefault();
     }
   });
   
   // id="rap"のボタンがクリックされたときの挙動
-  rap.addEventListener('click', function() {
+  rap.addEventListener('click', (e) => {
     // inputタグのvalue属性が"RESET"のときの挙動
     if (rap.value == 'RESET') {
       rap.value = 'RAP';
       stopWatchInitial();
       stoppingTime = 0;
       time.innerHTML = "00:00:00.000";
+      e.preventDefault();
 
     // inputタグのvalue属性が"RAP"のときの挙動
+    } else if (rap.classList.contains("inactive")) {
+      return null;
     } else if (rap.value == 'RAP') {
-      // ラップタイムをtableに追加していく
+      addRap();
+      e.preventDefault();
     }
   });
 }
 
-
-
-
-document.addEventListener('DOMContentLoaded', stopWatch)
+window.addEventListener('DOMContentLoaded', stopWatch);
