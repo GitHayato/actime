0e44e2594edb7ef7aece04d098c6fdabb168400a
GitHayato <gabayt0_0@icloud.com>
2021-02-05 09:44:09 +0900

    feat: ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   
   resources :watches, only: [:index, :new, :create]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only:[:index]
+  end
   resources :top_pages, only: [:index]
 end
