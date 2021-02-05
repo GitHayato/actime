36c8e24f01cb400672f0c1476a9198a870f3dee7
GitHayato <gabayt0_0@icloud.com>
2021-02-05 19:13:13 +0900

    feat: ルームの選択画面作成準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   
+  root to: "top_pages#index"
   resources :watches, only: [:index, :new, :create]
-  resources :rooms, only: [:new, :create] do
+  resources :rooms, only: [:index, :new, :create] do
     resources :messages, only:[:index]
   end
   resources :top_pages, only: [:index]
