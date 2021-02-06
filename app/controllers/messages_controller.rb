a2ba51c35e31ddc8ca53fae669eb37013949f3cd
GitHayato <gabayt0_0@icloud.com>
2021-02-06 16:26:15 +0900

    feat: メッセージ送信機能

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,20 @@ class MessagesController < ApplicationController
     # 自分の属するルームのみ表示
     @rooms = Room.all
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
 
+  def create
+    @room = Room.find(params[:room_id])
+    message = Message.create(message_params)
+    if message.save
+      redirect_to room_messages_path(@room.id)
+    end
+  end
+
+  private
+
+  def message_params
+    params.permit(:content).merge(user_id: current_user.id, room_id: @room.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: "top_pages#index"
   resources :watches, only: [:index, :new, :create]
   resources :rooms, only: [:index, :new, :create] do
-    resources :messages, only:[:index]
+    resources :messages, only:[:index, :create]
   end
   resources :top_pages, only: [:index]
 end
