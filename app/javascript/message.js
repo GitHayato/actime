3dcc61c628636f7db1435eef5f1c8b71866bf931
GitHayato <gabayt0_0@icloud.com>
2021-02-09 16:25:44 +0900

    feat: momentを使った投稿時間フォーマット変更

--- app/javascript/message.js
+++ app/javascript/message.js
@@ -1,3 +1,5 @@
+const moment = require("moment");
+
 function sendMessage() {
   const submit = document.getElementById('submit');
   submit.addEventListener('click', (e) => {
@@ -15,12 +17,14 @@ function sendMessage() {
         const message = XHR.response.post;
         const list = document.getElementById("list");
         const formText = document.getElementById("send-input");
+        const createMoment = moment(message.created_at, 'YYYY-MM-DDTHH:mm:ssZ')
+        const createTime = createMoment.format('MM/DD HH:mm');
         const HTML =`
           <div class="message">
             <div class="information">
               <div class="message-info">
                 <p class="info-user">${message.user_id}</p>
-                <p class="info-date">${message.created_at}</p>
+                <p class="info-date">${createTime}</p>
               </div>
               <ul class="navigation">
                 <li><img src="/assets/three_point.png" class="menu-tag" resize="20*20">
