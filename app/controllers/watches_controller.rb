48f1ca0e2dc97cb1bb885f5b53f0ba5e77d6db10
GitHayato <gabayt0_0@icloud.com>
2021-02-24 17:06:15 +0900

    Revert "fix: watchDB空のときのview修正"
    This reverts commit 8f136873b13d93142f8ab818df279d1bfebb1076.

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -6,10 +6,7 @@ class WatchesController < ApplicationController
 
   def new
     @room = Room.find(params[:room_id])
-    @watches = Watch.where(room_id: params[:room_id]).exists?
-    if @watches
-      @watches = Watch.where(room_id: params[:room_id]).order(id: "DESC").page(params[:page]).per(50)
-    end
+    @watches = Watch.where(room_id: params[:room_id]).order(id: "DESC").page(params[:page]).per(50)
   end
 
   def create
