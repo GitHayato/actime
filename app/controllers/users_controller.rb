5269f48085ff03c54b689ffbbbe5043af9036560
GitHayato <gabayt0_0@icloud.com>
2021-03-06 19:00:10 +0900

    feat: パス設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: :show
   def show
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by(public_uid: params[:id])
   end
 end
