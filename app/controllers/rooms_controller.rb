6dca8b90f63cd5e2bdf2e4efe1a6b69603796719
GitHayato <gabayt0_0@icloud.com>
2021-03-06 15:09:38 +0900

    feat: room#editバックエンド作成

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,7 @@
 class RoomsController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+  before_action :user_except_current_user, only: [:new, :edit]
+
   def index
     current_user_rooms = RoomUser.where(user_id: current_user.id)
     rooms = []
@@ -10,7 +13,6 @@ class RoomsController < ApplicationController
 
   def new
     @room = Room.new
-    @users = User.where.not(id: current_user.id)
   end
 
   def create
@@ -19,6 +21,17 @@ class RoomsController < ApplicationController
       redirect_to room_messages_path(@room.public_uid)
     else
       render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @room.update(room_params)
+      redirect_to room_messages_path(@room.public_uid)
+    else
+      render :edit
     end
   end
 
@@ -33,5 +46,13 @@ class RoomsController < ApplicationController
   
   def room_params
     params.require(:room).permit(:thread_name, :public_uid, user_ids: [])
+  end
+
+  def set_user
+    @room = Room.find_by(public_uid: params[:id])
+  end
+
+  def user_except_current_user
+    @users = User.where.not(id: current_user.id)
   end
 end
