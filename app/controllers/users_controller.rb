44e0ae16ff73e9fd26310093e059f790799576b5
GitHayato <gabayt0_0@icloud.com>
2021-03-09 14:12:09 +0900

    feat: ユーザー削除バックエンド

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: :show
+  before_action :set_user, only: [:show, :destroy]
   def show
+  end
+
+  def destroy
+    if @user.destroy
+      redirect_to new_user_registration_path
+    else
+      render :show
+    end
   end
 
   private
