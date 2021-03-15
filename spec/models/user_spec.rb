d39e44ecc51d2b70ee53bbfd35165ef3c9a8783a
GitHayato <gabayt0_0@icloud.com>
2021-03-15 15:17:35 +0900

    feat: userモデル正常系単体テストコード

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,13 @@ RSpec.describe User, type: :model do
   before do
     @user = FactoryBot.build(:user)
   end
+
+  describe 'ユーザー情報' do
+    context '新規登録できる' do
+      it 'username, email, password, confirmationがすべてあるとき' do
+        @user
+        expect(@user).to be_valid
+      end
+    end
+  end
 end
