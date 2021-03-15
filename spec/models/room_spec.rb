3a1b706f79de7c5fb334f1ee4cc52e1bd9cc33d1
GitHayato <gabayt0_0@icloud.com>
2021-03-15 15:44:27 +0900

    feat: roomモデル正常系単体テストコード

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe Room, type: :model do
   before do
     @room = FactoryBot.build(:room)
   end
+  describe 'スレッド情報'　do
+    context 'スレッドを作成できる' do
+      it 'スレッド名があるとき' do
+        @room
+        expect(@room).to be_valid
+      end
+    end
+  end
 end
