0716c8fc8abc9c43d913a97d3e9f6af59721e6d6
GitHayato <gabayt0_0@icloud.com>
2021-03-16 14:01:22 +0900

    feat: room_userモデル異常系単体テストコード

--- spec/models/room_user_spec.rb
+++ spec/models/room_user_spec.rb
@@ -10,5 +10,17 @@ RSpec.describe RoomUser, type: :model do
         expect(@room_user).to be_valid
       end
     end
+    context '保存できない' do
+      it 'user_idがないとき' do
+        @room_user.user_id = ''
+        @room_user.valid?
+        expect(@room_user.errors.full_messages).to include "Userを入力してください"
+      end
+      it 'room_idがないとき' do
+        @room_user.room_id = ''
+        @room_user.valid? 
+        expect(@room_user.errors.full_messages).to include "Roomを入力してください"
+      end
+    end
   end
 end
