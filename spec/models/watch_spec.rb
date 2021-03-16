98b170c16909de0fb36bb8cf2f8e498a8d56da3c
GitHayato <gabayt0_0@icloud.com>
2021-03-16 14:23:14 +0900

    feat: watchモデル正常系単体テストコード

--- spec/models/watch_spec.rb
+++ spec/models/watch_spec.rb
@@ -4,4 +4,24 @@ RSpec.describe Watch, type: :model do
   before do
     @watch = FactoryBot.build(:watch)
   end
+
+  describe 'タイム情報' do
+    context 'タイムを保存' do
+      it 'タイムを保存できるとき' do
+        expect(@watch).to be_valid
+      end
+      it 'eventがなくても保存ができる' do
+        @watch.event = ''
+        expect(@watch).to be_valid
+      end
+      it 'distanceがなくても保存ができる' do
+        @watch.distance = ''
+        expect(@watch).to be_valid
+      end
+      it 'user_idがなくても保存ができる' do
+        @watch.user_id = ''
+        expect(@watch).to be_valid
+      end
+    end
+  end
 end
