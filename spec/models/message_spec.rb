5f34712954f7d8414dac3a7206930b8b1df16cad
GitHayato <gabayt0_0@icloud.com>
2021-03-16 14:10:43 +0900

    feat: messageモデル異常系単体テストコード

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -11,5 +11,22 @@ RSpec.describe Message, type: :model do
         expect(@message).to be_valid
       end
     end
+    context 'メッセージ送信できない' do
+      it 'contentがないとき' do
+        @message.content = ''
+        @message.valid?
+        expect(@message.errors.full_messages).to include "内容を入力してください"
+      end
+      it 'user_idがないとき' do
+        @message.user_id = ''
+        @message.valid?
+        expect(@message.errors.full_messages).to include "Userを入力してください"
+      end
+      it 'room_idがないとき' do
+        @message.room_id = ''
+        @message.valid?
+        expect(@message.errors.full_messages).to include "Roomを入力してください"
+      end
+    end
   end
 end
