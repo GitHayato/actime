db04262afb83f40f0568b84a0b1a006694401834
GitHayato <gabayt0_0@icloud.com>
2021-03-16 14:10:16 +0900

    feat: messageモデル正常系単体テストコード

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe Message, type: :model do
   before do
     @message = FactoryBot.build(:message)
   end
+
+  describe 'メッセージ情報' do
+    context 'メッセージ送信' do
+      it '送信できるとき' do
+        expect(@message).to be_valid
+      end
+    end
+  end
 end
