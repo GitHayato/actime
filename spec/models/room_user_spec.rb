64b091e0f6aa06742b1032d0bd073383f6d2a139
GitHayato <gabayt0_0@icloud.com>
2021-03-16 14:01:04 +0900

    feat: room_userモデル正常系単体テストコード

--- spec/models/room_user_spec.rb
+++ spec/models/room_user_spec.rb
@@ -4,4 +4,11 @@ RSpec.describe RoomUser, type: :model do
   before do
     @room_user = FactoryBot.build(:room_user)
   end
+  describe '中間テーブル情報' do
+    context '保存できる' do
+      it '外部キーが適切に保存されるとき' do
+        expect(@room_user).to be_valid
+      end
+    end
+  end
 end
