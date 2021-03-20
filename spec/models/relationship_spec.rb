ce412b9c3e448ec4f3ec35291cc637cd843bc180
GitHayato <gabayt0_0@icloud.com>
2021-03-20 14:04:43 +0900

    feat: relationshipモデル正常系単体テストコード

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe Relationship, type: :model do
   before do
     @relationship = FactoryBot.create(:relationship)
   end
+
+  describe 'ユーザーの関係性' do
+    context 'フォロー及びフォロー解除ができる' do
+      it '中間テーブルに保存ができる' do
+        expect(@relationship).to be_valid
+      end
+    end
+  end
 end
