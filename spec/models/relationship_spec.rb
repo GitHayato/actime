1c6129e84e8b8da61bfc9aacda913b12df6a0adf
GitHayato <gabayt0_0@icloud.com>
2021-03-20 14:05:56 +0900

    feat: relationshipモデル一意性検証単体テストコード

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -24,5 +24,26 @@ RSpec.describe Relationship, type: :model do
         expect(@relationship.errors.full_messages).to include "Followを入力してください"
       end
     end
+
+    context '一意性の検証' do
+      before do
+        @relation = FactoryBot.create(:relationship)
+        @relation2 = FactoryBot.build(:relationship)
+      end
+      it 'user_idが同じでもfollow_idが異なれば保存できる' do
+        @relation.follow_id = @relation2.follow_id
+        expect(@relation).to be_valid
+      end
+      it 'follow_idが同じでもuser_idが異なれば保存できる' do
+        @relation.user_id = @relation2.user_id
+        expect(@relation).to be_valid
+      end
+      it 'user_idとfollow_idの組み合わせは一意でなけば保存できない' do
+        @relation2.user_id = @relation.user_id
+        @relation2.follow_id = @relation.follow_id
+        @relation2.valid?
+        expect(@relation2.errors.full_messages).to include "Followはすでに存在します"
+      end
+    end
   end
 end
