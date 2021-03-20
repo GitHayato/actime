a70814db8068e16f93ea034a4e2124bfc2622b76
GitHayato <gabayt0_0@icloud.com>
2021-03-20 15:29:35 +0900

    feat: ユーザー新規登録異常系結合テストコード

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -32,4 +32,26 @@ RSpec.describe "ユーザー新規登録", type: :system do
       expect(page).to have_no_content("ログイン")
     end
   end
+
+  context 'ユーザー新規登録ができないとき' do
+    it '誤った情報を入力すれば新規登録ができずに新規登録ページに戻ってくる' do
+      # トップページに移動する
+      visit root_path
+      # 新規登録ボタンが有ることを確認する
+      expect(page).to have_content("新規登録")
+      # 新規登録画面に遷移
+      visit new_user_registration_path
+      # ユーザー情報を入力する
+      fill_in "ユーザー名", with: ''
+      fill_in "メールアドレス", with: ''
+      fill_in "パスワード", with: ''
+      fill_in "パスワード(確認用)", with: ''
+      # 登録ボタンを押してもユーザーモデルのカウントは変わらない
+      expect {
+        find('input[name="commit"]').click
+      }.to change {User.count}.by(0)
+      # 新規登録ページに戻される
+      expect(current_path).to eq(user_registration_path)
+    end
+  end
 end
