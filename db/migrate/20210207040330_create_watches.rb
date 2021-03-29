cf3ec34d6a0dd570397270eb4528b6ab4e5fddb8
GitHayato <gabayt0_0@icloud.com>
2021-03-29 19:49:28 +0900

    fix: マイグレーションファイルカラム削除

--- db/migrate/20210207040330_create_watches.rb
+++ db/migrate/20210207040330_create_watches.rb
@@ -2,8 +2,6 @@ class CreateWatches < ActiveRecord::Migration[6.0]
   def change
     create_table :watches do |t|
       t.string        :watch,    null: false
-      t.string        :event
-      t.string        :distance
       t.references    :user,     foreign_key: true
       t.references    :room,     foreign_key: true
       t.timestamps
