a3f41fb54baa98de304f4244e68b4271f9796005
GitHayato <gabayt0_0@icloud.com>
2021-03-30 13:34:23 +0900

    feat: watchマイグレーションファイル時系列変更

--- db/migrate/20210330040330_create_watches.rb
+++ db/migrate/20210330040330_create_watches.rb
@@ -4,6 +4,8 @@ class CreateWatches < ActiveRecord::Migration[6.0]
       t.string        :watch,    null: false
       t.references    :user,     foreign_key: true
       t.references    :room,     foreign_key: true
+      t.references    :event,    foreign_key: true
+      t.references    :distance, foreign_key: true
       t.timestamps
     end
   end
