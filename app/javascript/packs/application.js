c038ff50a5a8f66a495c548838acbc57f49e7d06
GitHayato <gabayt0_0@icloud.com>
2021-04-02 16:36:17 +0900

    feat: 非同期送信用JS作成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels")
 require("../watch")
 require("../message")
 require("../watchUpdate")
+require("../registerDetails")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
