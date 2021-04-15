6d6c254e997b36ab74c031e5e1a366fd7124686e
GitHayato <gabayt0_0@icloud.com>
2021-04-15 22:24:01 +0900

    feat: sns認証ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: "users/registrations",
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
   
   root to: "top_pages#index"
   resources :users, only: [:show, :edit, :update, :destroy] do
