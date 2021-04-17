6eadf8081c2092a1abb1da89421b98186026dd50
GitHayato <gabayt0_0@icloud.com>
2021-04-17 11:48:20 +0900

    feat: SNS認証バックエンド処理

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   has_many :rooms, through: :room_users
   has_many :messages, dependent: :destroy
   has_many :watches, dependent: :destroy
-  has_many :sns_credential
+  has_many :sns_credentials
 
   has_many :relationships, dependent: :destroy
   has_many :followings, through: :relationships, source: :follow
@@ -46,5 +46,51 @@ class User < ApplicationRecord
     if search != ""
       User.where('username LIKE(?)', "%#{search}%")
     end
+  end
+
+  def self.without_sns_data(auth)
+    user = User.where(email: auth.info.email).first
+    if user.present?
+      sns = SnsCredential.create(
+        uid: auth.uid,
+        provider: auth.provider,
+        user_id: user.id
+      )
+    else
+      user = User.new(
+        username: auth.info.name,
+        email: auth.info.email,
+      )
+      sns = SnsCredential.new(
+        uid: auth.uid,
+        provider: auth.provider
+      )
+    end
+    return { user: user ,sns: sns}
+  end
+
+  def self.with_sns_data(auth, snscredential)
+    user = User.where(id: snscredential.user_id).first
+    unless user.present?
+      user = User.new(
+        username: auth.info.name,
+        email: auth.info.email,
+      )
+    end
+    return {user: user}
+  end
+
+  def self.find_oauth(auth)
+    uid = auth.uid
+    provider = auth.provider
+    snscredential = SnsCredential.where(uid: uid, provider: provider).first
+    if snscredential.present?
+      user = with_sns_data(auth, snscredential)[:user]
+      sns = snscredential
+    else
+      user = without_sns_data(auth)[:user]
+      sns = without_sns_data(auth)[:sns]
+    end
+    return { user: user ,sns: sns}
   end
 end
